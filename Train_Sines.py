d42ef9721fde9c43d2eb744433f3b769d0b55d08
Ben Ballyk <69019882+BennyScallywag@users.noreply.github.com>
2024-07-15 00:36:36 +0100

    Added Parallelization

--- Train_Sines.py
+++ Train_Sines.py
@@ -1,4 +1,5 @@
 from checkpoint_torch_timegan import timegan
+from parallel_timegan import parallel_timegan
 from WTimeGAN import w_timegan
 import numpy as np
 from torch_dataloading import sine_data_generation
@@ -16,7 +17,7 @@ parameters['num_layer'] = 3
 parameters['iterations'] = 700
 parameters['batch_size'] = 128
 #generated_sine_data, e, r, s, d, g = w_timegan(sine_data, parameters, checkpoint_file='TEST3_sines_checkpoint.pth')
-generated_sine_data, e, r, s, d, g = timegan(sine_data, parameters, checkpoint_file='TEST4_sines_checkpoint.pth')
+generated_sine_data, e, r, s, d, g = parallel_timegan(sine_data, parameters, checkpoint_file='pTEST1_sines_checkpoint.pth')
 
 plot_original_vs_generated(sine_data, generated_sine_data, num_samples=15)
 
